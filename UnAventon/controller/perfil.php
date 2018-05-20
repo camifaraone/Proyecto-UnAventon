6b2d5baf6baccc9fba9a6c1020f674117b9f6bad
Mati <matute94.23@gmail.com>
2018-05-20 19:44:09 -0300

    pasaje de ID de usuario correctamente por url / en index, verperfil y registrarauto

--- UnAventon/controller/perfil.php
+++ UnAventon/controller/perfil.php
@@ -2,11 +2,13 @@
 
 
 require_once ("../model/perfil.php");
-if(isset($_GET["email"]) ){  //si el Email esta seteado por GET (en el buscador) , entonces llama a una funcion ubicada en "../model/perfil.php" y guarda el resultado en la variable $usuario
+if(isset($_GET["idautoincremental"]) ){  //si el Email esta seteado por GET (en el buscador) , entonces llama a una funcion ubicada en "../model/perfil.php" y guarda el resultado en la variable $usuario
 
-$usuario= get_perfil($_GET["email"]);
+$usuario= get_perfil($_GET["idautoincremental"]);
 
 }
+
+$id= ($_GET["idautoincremental"]);
 
 
 
