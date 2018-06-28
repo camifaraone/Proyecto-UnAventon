918616e014997fd273aaec82ba79ca0b97b3f544
Mati <matute94.23@gmail.com>
2018-06-27 23:20:55 -0300

    pagina actual funcionando

--- UnAventon/model/editarviaje.php
+++ UnAventon/model/editarviaje.php
@@ -42,14 +42,14 @@
  
 
  
-function editarviaje($fecha, $monto, $duracion, $hssalida, $observaciones, $id){
+function editarviaje($fecha, $monto, $duracion, $hssalida, $observaciones, $origen, $destino, $vehiculo, $id){
  require ("db.php");
 try{
 
-	$sql = $conn->prepare("UPDATE viaje SET fecha=? ,monto=? ,duracion=? ,hssalida=?, observaciones=? WHERE idviaje=?");
+	$sql = $conn->prepare("UPDATE viaje SET fecha=? ,monto=? ,duracion=? ,hssalida=?, observaciones=?, idOrigen=?, idDestino=?, idvehiculo=? WHERE idviaje=?");
 	
     
-	$sql->execute(array( $fecha, $monto, $duracion, $hssalida, $observaciones, $id));
+	$sql->execute(array( $fecha, $monto, $duracion, $hssalida, $observaciones, $origen, $destino, $vehiculo, $id));
 	
 
 } catch(PDOException $e){
@@ -63,6 +63,66 @@ try{
 return true;
 
 }
+
+function get_origen() {
+	 require "../model/db.php";//te crea una conexion
+	try{
+		$sql = $conn->prepare("select * from origen");
+		$sql ->execute();
+	
+	/*while( $datos = $sql->fetch() )
+    echo $datos[0]"ID".$datos[1].$datos[2].$datos[3] . '<br />';}*/
+
+	$result= $sql->fetchAll();
+	
+   }
+   catch(PDOException $e) {
+  $result= 'Error: ' . $e->getMessage();
+  }
+	return $result;
+	 
+ }
+ 
+ 
+ function get_destino() {
+	 require "../model/db.php";//te crea una conexion
+	try{
+		$sql = $conn->prepare("select * from destino ");
+		$sql ->execute();
+	
+	/*while( $datos = $sql->fetch() )
+    echo $datos[0]"ID".$datos[1].$datos[2].$datos[3] . '<br />';}*/
+
+	$result= $sql->fetchAll();
+	
+   }
+   catch(PDOException $e) {
+  $result= 'Error: ' . $e->getMessage();
+  }
+	return $result;
+	 
+ }
+ 
+ 
+ 
+ function get_vehiculo($iduser){
+	require ("db.php");
+	try{
+		$sql = $conn->prepare("select * from vehiculo where idautoincremental=:iduser");
+		$sql->bindParam(":iduser",$iduser,PDO::PARAM_INT);
+		$sql ->execute();
+	
+	/*while( $datos = $sql->fetch() )
+    echo $datos[0]"ID".$datos[1].$datos[2].$datos[3] . '<br />';}*/
+
+	$result= $sql->fetchAll();
+	
+
+   }catch(PDOException $e) {
+  $result= 'Error: ' . $e->getMessage();
+  }
+	return $result;
+ }
 
 
    
