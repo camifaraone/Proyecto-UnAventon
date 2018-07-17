9846b44f78e36fe876083f6c81cae06699357345
Camila Faraone <camilafaraone@gmail.com>
2018-07-17 05:25:21 -0300

    modificacion alerta eliminar cuenta

--- UnAventon/controller/eliminarcuenta.php
+++ UnAventon/controller/eliminarcuenta.php
@@ -7,7 +7,7 @@ $id = ($_GET['idautoincremental']);
 //$a = eliminar_cuenta ($id);
 
 // ALERTA QUE DIGA QUE SE ELIMINO CORRECTAMENTE EL USUARIO 
-echo "<html><script> confirm('Nos vemos pronto!');</script></html>"; 
+echo "<html><script> alert('Nos vemos pronto!');</script></html>"; 
 echo "<html><script> document.location.href='../controller/register.php';</script></html>"; 
  
 session_destroy;
