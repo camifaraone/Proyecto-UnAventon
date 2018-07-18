46202fe6e5707bc7850590ab49ceded261967546
Mati <matute94.23@gmail.com>
2018-07-18 18:37:28 -0300

    mod en verpostulaciones + buscador avanzado casi listo

--- UnAventon/controller/buscador.php
+++ UnAventon/controller/buscador.php
@@ -7,10 +7,10 @@ require_once ("../model/buscador.php");
 $id= ($_GET["idautoincremental"]);
 
 
-$busqueda = trim($_POST['buscador']);
-//var_dump($busqueda);
-//var_dump($_POST);
+$busqueda = trim($_POST['T1']);
 
+$buscador = get_busqueda ($busqueda);
+print_r($buscador);
 
 
  
