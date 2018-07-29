ee01ce58b61a2ba8d9dbde426766ac23c9ce742c
Felipe Pisoni <feli.pisoni@gmail.com>
2018-07-29 19:41:02 -0300

    Correcciones minimas en varios archivos

--- UnAventon/controller/eliminarcuenta.php
+++ UnAventon/controller/eliminarcuenta.php
@@ -5,6 +5,7 @@ require_once "../model/eliminarcuenta.php";
 $id = ($_GET['idautoincremental']);
 
 $a = eliminar_cuenta ($id);
+session_destroy;
 
 // ALERTA QUE DIGA QUE SE ELIMINO CORRECTAMENTE EL USUARIO 
 echo "<html><script> alert('Nos vemos pronto!');</script></html>"; 
