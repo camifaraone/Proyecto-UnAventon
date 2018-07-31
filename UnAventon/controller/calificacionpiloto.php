d5eac8ed9878daaec9d4a35ad94e220512c75a71
Mati <matute94.23@gmail.com>
2018-07-31 04:01:14 -0300

    empezando el calificacion piloto

--- UnAventon/controller/calificacionpiloto.php
+++ UnAventon/controller/calificacionpiloto.php
@@ -3,23 +3,41 @@
 require_once "../model/calificacionpiloto.php";
 
 
-if(isset($_GET["idautoincremental"]) ){  //si el Email esta seteado por GET (en el buscador) , entonces llama a una funcion ubicada en "../model/perfil.php" y guarda el resultado en la variable $usuario
 
-$usuario= get_perfil($_GET["idautoincremental"]);
-
-}
 
 $id= ($_GET["idautoincremental"]);
+$idviaje= ($_POST["idviaje"]);
+
+var_dump($idviaje);
+$calificacion = $_POST['calificacion'];
+$comentario= $_POST['comentario'];
+
+/*
+$piloto = get_id_piloto($idviaje);
+$comen = comentario_calificacion ($id, $idviaje, $comentario);
+$idcomentario = get_id_comentario($id,$idviaje);
+
+if ( $calificacion == 1) {
+	$calif = calificacion_positiva($calificacion,$id,$idviaje,$idcomentario);
+	$positiva = positiva_calificacion($piloto);
+}
+if( $calificacion == 2) { 
+	$calif = calificacion_neutral($calificacion,$id,$idviaje,$idcomentario);
+	
+}
+if( $calificacion == 3){
+	$calif = calificacion_mal($calificacion,$id,$idviaje,$idcomentario);
+	$negativa = negativa_calificacion($piloto);
+}
 
 
+header("Location: ../controller/misviajes.php?idautoincremental=".$id);
 
-
-
-
-
-
+*/
 
 include "../view/calificacionpiloto.html";
+
+
 	
 
 ?>
--- UnAventon/model/calificacionpiloto.php
+++ UnAventon/model/calificacionpiloto.php
@@ -1,23 +1,137 @@
 <?php
  
- function get_perfil($idautoincremental){
-	 require_once ("db.php");//te crea una conexion
+ function comentario_calificacion($id, $idviaje, $comentario){
+		
+	require("db.php");
 	try{
-		$sql = $conn->prepare("select * from usuario where idautoincremental=:idautoincremental"); //selecciona todos los campos del usuario que coincidan con el email que viene paso por GET ( en el buscador) 
-		$sql->bindParam(":idautoincremental",$idautoincremental,PDO::PARAM_INT);   
-		$sql ->execute();
-	
-	
+		$sql= $conn->prepare("INSERT INTO comentario
+				(idautoincremental, idviaje, comentario ) 
+				VALUES(?, ?, ?)" );
+		
+		
+		
 
-	$result= $sql->fetch(); // los resultados de la consulta son guardados en un array 
+				
+		$sql ->execute(array("$id","idviaje","comentario"));
 	
-
-   }
-   catch(PDOException $e) {
-  $result= 'Error: ' . $e->getMessage();
-  }
-	return $result;
-	 
+    }catch(PDOException $e) {
+			return 'Error: ' . $e->getMessage();
+    }
+	return true;
+	
  }
+ 
+ function get_id_comentario($id, $idviaje){
+		
+	require("db.php");
+	try{
+		$sql= $conn->prepare("select idcomentario from comentario where idautoincremental=? and idviaje=?" );
+		
+		
+		
+
+				
+		$sql ->execute(array("$id","idviaje"));
+		$result=$sql->fetchAll();
+	
+    }catch(PDOException $e) {
+			return 'Error: ' . $e->getMessage();
+    }
+	return true;
+	
+ }
+ 
+ function get_id_piloto($idviaje){
+		
+	require("db.php");
+	try{
+		$sql= $conn->prepare("select idautoincremental from usuario where idviaje=?" );
+		
+		
+		
+
+				
+		$sql ->execute(array("idviaje"));
+	
+	
+    }catch(PDOException $e) {
+			return 'Error: ' . $e->getMessage();
+    }
+	return true;
+	
+ }
+ 
+ 
+ 
+ 
+function calificacion_positiva($calificacion, $id, $idviaje, $idcomentario){
+		
+	require("db.php");
+	try{
+		$sql= $conn->prepare("INSERT INTO calificacionpiloto
+				(puntaje, idcomentario, idautoincremental, idviaje) 
+				VALUES(?, ?, ?, ?)" );
+		
+		
+		
+
+				
+		$sql ->execute(array("$calificacion","idcomentario","id","idviaje"));
+	
+    }catch(PDOException $e) {
+			return 'Error: ' . $e->getMessage();
+    }
+	return true;
+	
+ }
+ 
+ 
+function positiva_calificacion($piloto){
+ require "db.php";
+
+try{
+
+	$sql = $conn->prepare("UPDATE usuario SET califiacion=? WHERE idautoincremental=?");
+	
+	
+
+	$sql->execute(array($calificacion + 1,"$piloto"));
+	
+
+} catch(PDOException $e){
+
+	return "ERROR: " . $e->getMessage();
+
+}
+
+
+
+return true;
+
+}
+
+function negativa_calificacion($piloto){
+ require "db.php";
+
+try{
+
+	$sql = $conn->prepare("UPDATE usuario SET califiacion=? WHERE idautoincremental=?");
+	
+	
+
+	$sql->execute(array($calificacion - 1,"$piloto"));
+	
+
+} catch(PDOException $e){
+
+	return "ERROR: " . $e->getMessage();
+
+}
+
+
+
+return true;
+
+}
 
  ?>
