04ac57a4f93e26a3db3c5c62c8b73a2367eff6ea
Mati <matute94.23@gmail.com>
2018-07-31 02:19:11 -0300

    casi terminado el pago

--- UnAventon/controller/metododepago.php
+++ UnAventon/controller/metododepago.php
@@ -1,19 +1,18 @@
+
 <?php
 
 error_reporting(E_ALL ^ E_NOTICE); //no muestra el error
 
-
 require_once ("../model/metododepago.php");
-include "../view/metododepago.html";
+
 
 
 $id= ($_GET["idautoincremental"]);
 $idviaje = ($_GET["idviaje"]);
 
 
-// NO FUNCIONA CORRECTAMENTE
 
-/*
+
 //if(isset($_POST["pago"])){
 	if(!empty($_POST['tipo']) && !empty($_POST['numtarjeta']) && !empty($_POST['mes']) && !empty($_POST['anio']) && !empty($_POST['nomape']) && !empty($_POST['codseguridad']) && !empty($_POST['documento'])) {
 	
@@ -24,32 +23,44 @@ $idviaje = ($_GET["idviaje"]);
 	$nomape=$_POST['nomape'];
 	$codseguridad=$_POST['codseguridad'];
 	$documento = $_POST['documento'];
-
-*/
 	
-	$a = estadopago($idviaje);
 	
-	if($a == true){
-	//$viaje = pago($tipo, $numtarjeta, $mes, $anio, $nomape,$codseguridad,$documento,$idviaje,$id);
-	echo "<html><script> alert('Pago realizado con éxito!');</script></html>"; 
-echo "<html><script> document.location.href='../controller/newtrip.php?idautoincremental=".$id."';</script></html>";  
- 
-    //header("Location: ../controller/perfil.php?idautoincremental=".$id);
-
-
-} else {
 	if ($mes < 9 && $anio < 18){
-     echo "<html><script> alert('Pago denegado!');</script></html>"; 
-echo "<html><script> document.location.href='../controller/metododepago.php?idautoincremental=".$id."';</script></html>";  
-}
-
+		
+		echo "<html><script> alert('Pago denegado!');</script></html>"; 
+		echo "<html><script> document.location.href='../controller/metododepago.php?idautoincremental=".$id."&&idviaje=".$idviaje."';</script></html>";
+	}
+	else {
+		$a = estadopago($idviaje);
+		$b = dame_el_id($idviaje);
+	
+	echo "<html><script> alert('Pago realizado con éxito!');</script></html>"; 
+	echo "<html><script> document.location.href='../controller/realizarpago.php?idautoincremental=".$b."';</script></html>";
+	}
+	}
+	
+	
+	/*
+	if ($mes < 9 && $anio < 18){
+		echo "<html><script> alert('Pago denegado!');</script></html>"; 
+		echo "<html><script> document.location.href='../controller/metododepago.php?idautoincremental=".$id."&&idviaje=".$idviaje."';</script></html>";
+	}
+	else {
+		
+		$a = estadopago($idviaje);
+		echo "<html><script> alert('Pago realizado con éxito!');</script></html>"; 
+		echo "<html><script> document.location.href='../controller/realizarpago.php?idautoincremental=".$id."';</script></html>";
 	}
 
+	}
+	else{
+		$message = "Todos los campos no deben de estar vacios!";
+	}
+*/
 
-//}
 
-
-
+include "../view/metododepago.html";
+if (!empty($message)) {echo "<p class=\"error\">" . "Mensaje: ". $message . "</p>";}
 
 
 ?>
--- UnAventon/model/metododepago.php
+++ UnAventon/model/metododepago.php
@@ -104,7 +104,25 @@ return true;
 
 }
 
+function dame_el_id($idviaje) {
+	 require "../model/db.php";//te crea una conexion
+	try{
+		
+		$sql = $conn->prepare("select idautoincremental from viaje where idviaje=:idviaje");
+		$sql->bindParam(":idviaje",$idviaje,PDO::PARAM_INT);
+		$sql ->execute();
+	
+	/*while( $datos = $sql->fetch() )
+    echo $datos[0]"ID".$datos[1].$datos[2].$datos[3] . '<br />';}*/
 
+	$result= $sql->fetchAll()[0][0];
+	
+   }
+   catch(PDOException $e) {
+  $result= 'Error: ' . $e->getMessage();
+  }
+	return $result;	 
+ }
 
 
 
